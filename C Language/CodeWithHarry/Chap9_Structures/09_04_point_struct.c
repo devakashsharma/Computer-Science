348529e5d1d75273a51e3b703e6322a8f6ba2b18
Akash Sharma <127339771+SharmaAkash001@users.noreply.github.com>
2023-10-24 12:30:40 +0530

    Update 09_04_point_struct.c

--- C Language/CodeWithHarry/Chap9_Structures/09_04_point_struct.c
+++ C Language/CodeWithHarry/Chap9_Structures/09_04_point_struct.c
@@ -8,7 +8,13 @@ struct employee {
 };
 
 int main() {
+    struct employee em1;
+    struct employee *ptr;
+    ptr = &em1;
+    // (*ptr).code = 23; // we can also used this to defined the variable
+    ptr->code = 23; // arrow operator automatically add the . and * at starting
     
+    printf("Code is %d", em1.code);
 
     return 0;
 }

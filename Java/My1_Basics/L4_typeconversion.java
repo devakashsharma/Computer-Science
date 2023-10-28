848a07398fbd5d3da4e859d87120f2fc36834eb2
Akash Sharma <127339771+devakashsharma@users.noreply.github.com>
2023-10-28 14:57:09 +0530

    Update L4_typeconversion.java

--- Java/My1_Basics/L4_typeconversion.java
+++ Java/My1_Basics/L4_typeconversion.java
@@ -1,11 +1,29 @@
-package Telusko.My1_Basics;
+package Telsuko.My1_Basics;
 
-public class L4_typeconversion {
+public class L4_typecasting {
     public static void main(String[] args) {
+
+        // casting is explicit conversion
+        // conversions is automatic casting
+
         float fnum = 3.5f;
         int num = (int)fnum;
         System.out.println(num);
 
+        int inum = 340;
+        byte bnum = (byte) inum; // it uses the '%' operator in order to make it into it's  range
+        System.out.println(bnum);
+
+        float fnum2 = 343.53f;
+        byte bynum = (byte) fnum2;
+        System.out.println(bynum);
+
+        byte a = 20;
+        byte b = 30;
+        int result = a * b;
+        // but a and b are byte so how could this be happened??
+        // so in java if we perform any operations and that's exceed the range then it promotes their type and this is
+        // also known as type promotion
+        System.out.println(result);
     }
-    
 }

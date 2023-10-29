bfff5f67169624a4e03b3bb8f497ce49dd4e57b2
Akash Sharma <127339771+devakashsharma@users.noreply.github.com>
2023-10-29 22:45:33 +0530

    add switch

---
+++ Java/My2_Conditional/L9_switch.java
@@ -0,0 +1,29 @@
+package Telsuko.My2_Conditional;
+
+public class L9_switch {
+    public static void main(String[] args) {
+        int num = 4;
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Sunday");
+        }
+    }
+}

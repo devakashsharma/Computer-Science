de81bc478d090da2bcdef4a415eafd99c67b7b10
Akash Sharma <127339771+devakashsharma@users.noreply.github.com>
2023-10-29 11:41:55 +0530

    added if else conditions

---
+++ Java/My2_Conditional/L6_if_else.java
@@ -0,0 +1,26 @@
+package Telsuko.My2_Conditional;
+
+public class L6_if_else
+{
+    public static void main(String[] args)
+    {
+        int a = 15;
+
+        if (a > 10 && a <= 20)
+        {
+            System.out.println("Hello There");
+        }
+
+        int x = 4;
+        int y = 7;
+
+        if (x > y)
+        {
+            System.out.println(x);
+        }
+        else
+        {
+            System.out.println(y);
+        }
+    }
+}

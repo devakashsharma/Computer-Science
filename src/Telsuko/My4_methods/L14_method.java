aacfc13568daba05c0c8ac85e16b00fd6bd4c20c
devakashsharma <soulakash20002@gmail.com>
2023-11-02 20:51:17 +0530

    added file

---
+++ src/Telsuko/My4_methods/L14_method.java
@@ -0,0 +1,22 @@
+package Telsuko.My4_methods;
+
+class JavaMethods {
+    public void playMusic() {
+        System.out.println("Music Playing. . . . .");
+    }
+
+    public String buyPen(int cost) {
+        if (cost >= 10)
+            return "Pen";
+
+        return "Sorry";
+    }
+}
+public class L14_method {
+    public static void main(String[] args) {
+        JavaMethods jame = new JavaMethods();
+        jame.playMusic();
+        String pen = jame.buyPen(20);
+        System.out.println(pen);
+    }
+}

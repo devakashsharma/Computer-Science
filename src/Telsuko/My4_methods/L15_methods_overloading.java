107c520b141a13e3a1145cc90bb183f5f9d70ff1
devakashsharma <soulakash20002@gmail.com>
2023-11-02 14:17:52 +0530

    added file

---
+++ src/Telsuko/My4_methods/L15_methods_overloading.java
@@ -0,0 +1,26 @@
+package Telsuko.My4_methods;
+
+class Calculator {
+    // int num; // here it is instances variable
+    public int add(int n1, int n2) {
+        // here n1 and n2 are local variable
+        return n1 + n2;
+    }
+    public int add(int n1, int n2, int n3) {
+        return n1 + n2 + n3;
+    }
+    public double add(double n1, int n2) {
+        return n1 + n2;
+    }
+}
+public class L15_methods_overloading {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        int num1 = cal.add(2, 3);
+        int num2 = cal.add(4, 6, 2);
+        double num3 = cal.add(34.2, 34);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}

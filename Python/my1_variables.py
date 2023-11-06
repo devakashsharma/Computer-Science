e169a0bf2332a71e895626a91c651dc5011b4474
devakashsharma <soulakash20002@gmail.com>
2023-11-06 20:29:11 +0530

    Python Added

---
+++ Python/my1_variables.py
@@ -0,0 +1,10 @@
+# print("Hello  World")
+
+# x = 34
+# y = 34.98
+# z = 'zek'
+# is_correct = True
+
+x, y, z, is_correct = (23, 34.34, 'name', True) # Multiple assignment
+print("Akash Sharma")
+print(x, y, z, is_correct)

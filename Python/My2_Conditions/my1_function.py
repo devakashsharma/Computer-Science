f567c373011747e83be5b3f3ac43ba29b7388fe3
devakashsharma <soulakash20002@gmail.com>
2023-11-07 19:49:10 +0530

    Updation in Function

--- Python/My2_Conditions/my1_function.py
+++ Python/My2_Conditions/my1_function.py
@@ -1,7 +1,23 @@
 # A function is a block of code, which is only perform when it is called
 
 # create a function
-def welcome(name="user"):
-    print(f'Hello There, {name}')
+def welcome(name='user'):
+    print(f"Hello, {name}")
 
-welcome(Akash)
+# name = input("Enter Your Name: ")
+# welcome(name)
+
+# return values
+def add_two(num1, num2):
+    total = num1 + num2
+    return total
+
+total = add_two(23, 43)
+print(total)
+
+# A lambda is a small anonymous funtion.
+# A lambda function can take any number of arguments, but can only have one expression.
+
+getSum = lambda num1, num2 : num1 + num2
+
+print(getSum(23, 34))

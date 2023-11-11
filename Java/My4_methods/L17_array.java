5b283d22a7760b9914ae63ced781f1d68d491490
devakashsharma <soulakash20002@gmail.com>
2023-11-11 16:19:22 +0530

    Array Added

--- Java/My4_methods/L17_array.java
+++ Java/My4_methods/L17_array.java
@@ -1,2 +1,18 @@
 package Java.My4_methods;
 
+public class L17_array {
+    public static void main(String[] args) {
+        int num[] = {5, 2, 5, 1};
+        // System.out.println(num[1]);
+
+        // for each
+        // for (int elements : num) {
+        //     System.out.print(elements + " ");
+        // }
+
+        // for loop
+        for(int i = 0; i < 4; i++) {
+            System.out.print(num[i] + " ");
+        }
+    }
+}

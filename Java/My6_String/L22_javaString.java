01adb196d3f308dccf1dd72a31cb7cd4a3b7f853
devakashsharma <soulakash20002@gmail.com>
2023-11-12 23:19:46 +0530

    string declare

--- Java/My6_String/L22_javaString.java
+++ Java/My6_String/L22_javaString.java
@@ -2,6 +2,13 @@ package Java.My6_String;
 
 public class L22_javaString {
     public static void main(String[] args) {
+        // String name = new String("Akash");
+        String name = "Akash";
+        System.out.println(name);
         
+        // System.out.println(name.hashCode());
+        System.out.println("Hello " + name);
+        System.out.println(name.charAt(2));
+        System.out.println(name.concat(" Sharma"));
     }
 }

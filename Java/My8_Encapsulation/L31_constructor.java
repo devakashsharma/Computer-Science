f7bb23362666ff920f9f73e801955e76a9b87230
devakashsharma <soulakash20002@gmail.com>
2023-11-14 00:44:44 +0530

    constructor

---
+++ Java/My8_Encapsulation/L31_constructor.java
@@ -0,0 +1,42 @@
+package Java.My8_Encapsulation;
+
+class Humans2 {
+    private String name;
+    private int age;
+
+    // constructor is a special method, it has the same name as the class name, and also it doesn't return anything
+    // everytime we create an object, constructor will be called
+    public Humans2() { // default constructor
+        name = "Akash";
+        age = 21;
+    }
+
+    public Humans2(int a, String n) { // parameterized constructor
+        name = n;
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class L31_constructor {
+    public static void main(String[] args) {
+        Humans2 obj = new Humans2();
+        Humans2 obj2 = new Humans2(30, "Akash");
+        System.out.println(obj2.getName() + " : " + obj2.getAge());
+    }
+}

39c1e0eec7e3818bfc5d255abb8a7364fef5c34e
devakashsharma <soulakash20002@gmail.com>
2023-11-15 21:48:46 +0530

    hybrid inheritance

---
+++ Java/MyAC_OOPS/Inheritance/myClass8_hybrid.java
@@ -0,0 +1,7 @@
+package Java.MyAC_OOPS.Inheritance;
+
+public class myClass8_hybrid {
+    public static void main(String[] args) {
+        // It is the combination of all the inheritance, means at some place there will be single level inheritance, or multi level, or hierarchial inheritance
+    }
+}

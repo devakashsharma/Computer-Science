b9d73f445430fd21dabbea4d0d05d3b707734148
devakashsharma <soulakash20002@gmail.com>
2023-11-16 15:49:53 +0530

    Interface

--- Java/MyAC_OOPS/myClass11_interface.java
+++ Java/MyAC_OOPS/myClass11_interface.java
@@ -1,17 +1,18 @@
 package Java.MyAC_OOPS;
 
-interface Animal1{
-    public void walk();
+interface Animal1 {
+    void walk();
 }
 
 interface Herbivore {
-
 }
+
 class Horse1 implements Animal1, Herbivore { // multiple interface
     public void walk() {
         System.out.println("walks on 4 legs");
     }
 }
+
 public class myClass11_interface {
     public static void main(String[] args) {
         Horse1 horse1 = new Horse1();

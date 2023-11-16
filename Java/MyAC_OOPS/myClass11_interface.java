3cddfadd4576a6f94cbabf037e6950e6911f6584
devakashsharma <soulakash20002@gmail.com>
2023-11-16 15:58:47 +0530

    Interface

--- Java/MyAC_OOPS/myClass11_interface.java
+++ Java/MyAC_OOPS/myClass11_interface.java
@@ -1,13 +1,14 @@
 package Java.MyAC_OOPS;
 
 interface Animal1 {
+    int eyes = 2; // we can change the value, if we declared, it will be static meean value will be common to all class, it will be availabel to all (public) and the value will be fixed, we cannot change it in the future
     void walk();
 }
 
 interface Herbivore {
 }
 
-class Horse1 implements Animal1, Herbivore { // multiple interface
+class Horse1 implements Animal1, Herbivore { // multiple inheritance
     public void walk() {
         System.out.println("walks on 4 legs");
     }

2229f2e9769cc92623c2328450b6a116c962dcda
devakashsharma <soulakash20002@gmail.com>
2023-11-16 18:54:16 +0530

    JavaScript File Created

---
+++ JavaScript/01 Fundamentals/01_variables.js
@@ -0,0 +1,11 @@
+console.log("Hello There!")
+
+const accountId = 2324;
+let accountEmail = "new@gamils.com"
+var accountMail = "old@gamisls.com"
+accountPassword = "12345"
+console.log(accountId);
+
+// isn't it a tedious task to print the variables name and values with the help of console, let's do in a different way
+
+console.table([accountId, accountEmail, accountMail, accountPassword]);

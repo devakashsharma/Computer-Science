7540e70fb14be147cfe0593a09e1bc2ee3d31c6e
devakashsharma <soulakash20002@gmail.com>
2023-11-16 19:05:28 +0530

    Data Types

---
+++ JavaScript/01 Fundamentals/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // it will treat all js code as newer version
+
+// data types
+
+// primitive 
+
+// number = 2 to power 53
+// bigint = 
+// string = ""
+// boolean = true/false
+// null = standalone value // but if you print you may find it is object
+// undefined 
+// symbol = unique
+
+console.log(typeof "Akash");
+console.log(typeof null);
+console.log(typeof undefined);
+
+// objects

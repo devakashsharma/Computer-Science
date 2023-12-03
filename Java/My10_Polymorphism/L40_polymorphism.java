5960a873c4c365ace23d597ca552d54d3763fec3
devakashsharma <soulakash20002@gmail.com>
2023-12-04 00:10:29 +0530

    Polymorphism

---
+++ Java/My10_Polymorphism/L40_polymorphism.java
@@ -0,0 +1,9 @@
+package Java.My10_Polymorphism;
+
+public class L40_polymorphism {
+    public static void main(String[] args) {
+        // Poly - Many, Morphism - Behaviour
+        // compile time polymorphism is also known as early binding ph
+        // Run time ph is also known as late binding ph
+    }
+}

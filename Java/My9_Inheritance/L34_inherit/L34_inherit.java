24b0a4657654840622e51998deaa2e46b29555da
devakashsharma <soulakash20002@gmail.com>
2023-12-03 16:53:59 +0530

    Multi-level inheritance

--- Java/My9_Inheritance/L34_inherit/L34_inherit.java
+++ Java/My9_Inheritance/L34_inherit/L34_inherit.java
@@ -2,12 +2,16 @@ package Java.My9_Inheritance.L34_inherit;
 
 public class L34_inherit {
     public static void main(String[] args) {
-        //        L34_inherit_Calc obj = new L34_inherit_Calc();
-                L34_inherit_AdvCalc obj = new L34_inherit_AdvCalc();
-                int n1 = obj.add(2,4);
-                int n2 = obj.sub(9,3);
-                int n3 = obj.multi(2, 3);
-                int n4 = obj.div(24, 4);
-                System.out.println("[" + n1 + ", " + n2 + ", " + n3 + ", " + n4 + "]");
-            }
+        // L34_inherit_Calc obj = new L34_inherit_Calc();
+        // L34_inherit_AdvCalc obj = new L34_inherit_AdvCalc();
+        L35_inherit_SupCal obj = new L35_inherit_SupCal();
+        int n1 = obj.add(2,4);
+        int n2 = obj.sub(9,3);
+        int n3 = obj.multi(2, 3);
+        int n4 = obj.div(24, 4);
+        double n5 = obj.power(4, 2);
+
+        System.out.println("[" + n1 + ", " + n2 + ", " + n3 + ", " + n4 + ", " + n5+ "]");
+    
+    }
 }
---
+++ Java/My9_Inheritance/L35_inherit_SupCal.java
@@ -0,0 +1,7 @@
+package Java.My9_Inheritance;
+
+public class L35_inherit_SupCal {
+    public double power (int n1, int n2) {
+        return Math.pow(n1, n2);
+    }
+}

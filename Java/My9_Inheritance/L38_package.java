860ba216334151ac368d5a6c55f6ccaf69a83a1b
devakashsharma <soulakash20002@gmail.com>
2023-12-03 20:07:10 +0530

    Package

---
+++ Java/My9_Inheritance/L38_package.java
@@ -0,0 +1,11 @@
+package Java.My9_Inheritance;
+
+// import ApnaCollege.OOPs.Pack.bank.*;
+// * is use in package to import all the files
+public class L38_package {
+    public static void main(String[] args) {
+        Account accObj = new Account();
+        accObj.name = "Name";
+        System.out.println(accObj.name);
+    }
+}

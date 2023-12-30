e6d803304b2157e4111ba260e304221ab9c2fbe0
devakashsharma <soulakash20002@gmail.com>
2023-12-30 23:29:49 +0530

    Numbers

---
+++ JavaScript/01_Fundamentals/08_nums_maths.js
@@ -0,0 +1,16 @@
+const score = 300;
+console.log(score);
+
+const balance = new Number(500);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(typeof (balance.toString()));
+// console.log(typeof balance.toString());
+console.log(balance.toString().length);
+
+const value = 343.4353;
+console.log(value);
+console.log(value.toFixed(2));
+console.log(value.toPrecision(3));
+console.log(value.toPrecision(2));

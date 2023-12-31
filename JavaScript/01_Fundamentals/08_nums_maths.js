0980cb8dd3cedb5470824f55177ac4e0680ed76a
devakashsharma <soulakash20002@gmail.com>
2023-12-31 13:27:32 +0530

    JS Numbers and Maths

--- JavaScript/01_Fundamentals/08_nums_maths.js
+++ JavaScript/01_Fundamentals/08_nums_maths.js
@@ -1,3 +1,5 @@
+// ---------------------------------- Numbers ----------------------------------------
+
 const score = 300;
 console.log(score);
 
@@ -14,3 +16,26 @@ console.log(value);
 console.log(value.toFixed(2));
 console.log(value.toPrecision(3));
 console.log(value.toPrecision(2));
+
+const num = 1000000;
+console.log(num.toLocaleString('en-IN'));
+
+// ---------------------------------- Maths ----------------------------------------
+
+console.log(Math);
+console.log(Math.abs(-345)); // it convert negative value to positive
+console.log(Math.round(4.4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.5)); // if a number is even greater than 0.1 then it will return next number, here 4.5 will be 5, if 4.1 = 5
+console.log((Math.floor(4.5))); // floor is the opposite of ceil, if num is 4.5 = 4, if 4.9 = 4, if 4.1 = 4
+console.log(Math.min(3, 5, 4, 7, 8, 1, 9));
+console.log(Math.max(3, 5, 4, 7, 8, 1, 9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.round(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);

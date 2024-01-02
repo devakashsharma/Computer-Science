fd7d0fba4110e5e8cda32e5d443d348425e84d49
devakashsharma <soulakash20002@gmail.com>
2024-01-02 17:19:20 +0530

    Switch

---
+++ JavaScript/04_control_flow/02_switch.js
@@ -0,0 +1,43 @@
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    default:
+        console.log("Nothing Match");
+        break;
+}
+
+const monthName = "Jan";
+switch (monthName) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+
+    default:
+        console.log("Nothing Match");
+        break;
+}

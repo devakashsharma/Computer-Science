451e8bbf66dc116d13067aecac4143acaf77435b
devakashsharma <soulakash20002@gmail.com>
2024-01-03 17:37:07 +0530

    while loop

---
+++ JavaScript/05_iterations/02_while.js
@@ -0,0 +1,14 @@
+// while
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i is ${i}`);
+//     i = i + 2;
+// }
+
+let myArr = ["Ethan", "Skywalker", "William"];
+let indexArr = 0;
+while(indexArr < myArr.length) {
+    console.log(myArr[indexArr]);
+    indexArr++;
+}

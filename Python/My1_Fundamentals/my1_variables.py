154689ae14c04deb5c033a7f1c51d632cc1f5104
devakashsharma <soulakash20002@gmail.com>
2024-01-31 20:07:10 +0530

    Fundamentals

--- Python/My1_Fundamentals/my1_variables.py
+++ Python/My1_Fundamentals/my1_variables.py
@@ -29,3 +29,40 @@ print(line2)
 print(line3)
 print(line2)
 print(line1)
+
+
+
+# CS50 - Intro to PY - 1
+
+# csName = input("Enter name: ")
+# print("Hello, ")
+# print(csName)
+
+#But if i want to print the name at the same line as hello have, then i can do this
+# print("hello, ", end='')
+# # print(csName)
+# so here the end='' is showing that there will no automatic next line, in the behind of the code it is in the form of end='\n' 
+
+
+# print("hello,", csName, sep='')
+# so now there will not be any space because of sep=''
+
+print("Hello, \"World\"")
+
+n = "Neo"
+# print(f"Hello, {n}")
+
+newName = input("Enter name: ").strip().title()
+
+# remove whitespace
+# newName = newName.strip()
+
+# capital the first letter of the sentence
+# newName = newName.capitalize()
+
+# capital al the first letter of any word
+# newName = newName.title()
+
+# newName = newName.strip().title()
+
+print(f"hello, {newName}")

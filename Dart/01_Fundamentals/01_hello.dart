c9cd79d954a76a93180e4aebaeb8e5d8f8828820
devakashsharma <soulakash20002@gmail.com>
2024-02-02 17:24:20 +0530

    Fundamentals

---
+++ Dart/01_Fundamentals/01_hello.dart
@@ -0,0 +1,14 @@
+// Hello There!
+void main(){
+  print("Hello There");
+}
+
+// Variables
+// In dart we just need to use 'var' for declaring the variables
+var name = "Akash";
+var sem = 3;
+var float = 3.6;
+
+// In languages like Dart, variables don't directly contain the values; 
+// instead, they hold references to objects in memory. When you assign a value to a variable, 
+// you are actually assigning a reference to the memory location where the value (object) is stored.

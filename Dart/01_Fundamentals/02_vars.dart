6fd975a4140b155e6bd8d3bebe400682e5a03d09
devakashsharma <soulakash20002@gmail.com>
2024-02-02 17:44:41 +0530

    Variables

--- Dart/01_Fundamentals/02_vars.dart
+++ Dart/01_Fundamentals/02_vars.dart
@@ -19,16 +19,23 @@ main () {
 
   // Data Type
   int num = 21;
+  print("num $num");
+
   var num2 = 20;
+  print("var $num2");
 
   String name1 = "Dart";
+  print("String $name1");
 
   double newNum = 232.4;
+  print("newNum $newNum");
 
   bool isTrue = true;
+  print("isTrue $isTrue");
 
   dynamic username = 123; // we can change it later
   username = "dartuser1";
+  print("Dynamic $username");
 
 }
 

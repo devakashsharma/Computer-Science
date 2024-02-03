61e593dabc4b192a3af5845ef3f2c1b1d230be54
devakashsharma <soulakash20002@gmail.com>
2024-02-03 16:19:44 +0530

    Find Unique

---
+++ Data-Structure/DS101_Array/My03_unique.java
@@ -0,0 +1,41 @@
+package Data-Structure.DS101_Array;
+import java.util.Scanner;
+
+public class My03_unique {
+    static int unique(int[] arr) {
+        int size = arr.length;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = -1;
+                    arr[j] = -1;
+                }
+            }
+        }
+
+        int unique = -1;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] != -1) {
+                unique = arr[i];
+            }
+        }
+
+        return unique;
+    }
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int size = userInput.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = userInput.nextInt();
+        }
+
+        int unique = unique(arr);
+        System.out.println(unique);
+    }
+}

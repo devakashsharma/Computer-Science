46e6a6add8e911ffc2bfe738d776dc6e74668e8d
devakashsharma <soulakash20002@gmail.com>
2024-02-10 21:07:02 +0530

    String

--- Dart/01_Fundamentals/03_string.dart
+++ Dart/01_Fundamentals/03_string.dart
@@ -12,7 +12,7 @@ void main() {
 
   // Raw string
   /// if we want to use special characters in a string such as \n then we need to use 'r'before the strings value
-  var rawString = r"Hey there \nThis is raw string";
+  var rawString = r"Hey there \n This is raw string";
   print(rawString);
 
   // Multi line string
@@ -21,12 +21,36 @@ void main() {
     Its me
     Dart user
     ''';
+    print(multi);
 
   var newMulti = """
     Hey There
     Its me
     Dart user
     """;
+    print(newMulti);
+
+  // String interpolation
+  var strAge = 20;
+  var strName = "STR";
+  print("The name is $strName and age is $strAge");
+
+  // Type conversion
+
+  // str -> int
+  var int1 = int.parse("12");
+  assert(int1 == 12); // it will check if it is converted or not
+
+  // int -> double
+  var int2 = double.parse("12.5");
+  assert(int2 == 12.5);
+
+  // int -> str
+  String ms = int1.toString();
+  print(ms);
+
+  // Typeof data type
+  print(ms.runtimeType); // it will check the data type
 
 
 

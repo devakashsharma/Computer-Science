464df83b8a6afa7af8a6fd728c434b4fb4d418ab
devakashsharma <soulakash20002@gmail.com>
2024-02-14 18:02:31 +0530

    React's component

---
+++ React/01_ViteReact/src/NewApp.jsx
@@ -0,0 +1,10 @@
+// if we create a react app by using 'create app' then we can use .js but since we are using 'vite react' then we need to use .jsx extension
+// one most important is that the function name should start with an uppercase.
+
+function NewApps() {
+    return (
+        <h2>React app with vite</h2>
+    )
+}
+
+export default NewApps;

e00367ca0eb5db45885ceffdec4b5099e3d4a845
devakashsharma <soulakash20002@gmail.com>
2024-03-30 15:39:15 +0530

    Custom React

--- React/01_ViteReact/src/App.jsx
+++ React/01_ViteReact/src/App.jsx
@@ -2,12 +2,15 @@ import NewApps from "./NewApp"
 
 function App() {
 
+  // we can assign a value in a html
+  const userName = "user_name";
+
   return (
     // <h1>Learning React with Vite</h1>
     // we can only return one tag, it could be NewApps, h1, h2, p etc, but it will take only one tag, but there is a trick....... and we call it fragment
     <>
         <NewApps />
-        <h2>New H2</h2>
+        <h2>New H2 - {userName}</h2>
         <p>Para, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi ipsam aut debitis totam! Sint temporibus minima, animi quod dicta sequi earum. Deserunt sint laborum aut dicta maxime, modi repudiandae.</p>
     </>
   )
--- React/01_ViteReact/src/main.jsx
+++ React/01_ViteReact/src/main.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// lets check a bit more
+function MyApp() {
+  return (
+    <h1>Custom App !</h1>
+  )
+}
+
+// Creating a custom React!
+
+// we can use this 
+// const reactElement = {
+//   type: "a",
+//   props: {
+//       href: "https://www.google.com/",
+//       target: "_blank"
+//   },
+//   children: "click me to visit google",
+// }
+
+// we have follow a specific syntax for custom react
+const ReactElement = React.createElement(
+  'a',
+  {
+    href: "https://www.google.com/",
+    target: "_blank"
+  },
+  "Click me to visit Google Baba"
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    // {/* <MyApp /> */}
+    ReactElement
+  // {/* </React.StrictMode>, */}
+
+  /*
+  // MyApp is a function we can run it by using () also
   <React.StrictMode>
-    <App />
+    MyApp()
   </React.StrictMode>,
+  */
 )

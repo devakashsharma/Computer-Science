41cbbd0b4e2c1b7a18154c37836a0e2e992bf432
devakashsharma <soulakash20002@gmail.com>
2024-04-14 02:17:32 +0530

    Cards

--- React/04_tailwindProps/src/App.jsx
+++ React/04_tailwindProps/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './index.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import Cards from "./component/Cards"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const myObj = {
+    username: "akash_kun",
+    age: 21,
+  }
+  const myArr = [1, 2, 3, 4, 5]
 
   return (
     <>
-      <h1 className='bg-red-300 text-yellow p-5'>Tailwind Testing!</h1>
+      <h1 className="bg-red-300 text-yellow p-5">Tailwind Testing!</h1>
+      <Cards username="Akash Shamra" myNewObj={myObj} btnText={"click me"}/>
+      <Cards username="Akash" myNewObj={myArr} btnText={"visit me"}/>
+      <Cards username="Skywalker" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

04c37ae41a124478be84bd3257c0a4d2120f06fc
devakashsharma <soulakash20002@gmail.com>
2024-05-12 18:44:14 +0530

    Navbar

--- Projects/WebDev/Nav Bar/nav.js
+++ Projects/WebDev/Nav Bar/nav.js
@@ -3,12 +3,12 @@ const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", () => {
     // console.log(links.classList);
+    // console.log(links.classList.contains("show-links"));
 
-    // to make :
-    // if(links.classList.contains("show-links")) {
+    // if (links.classList.contains("show-links")) {
     //     links.classList.remove("show-links");
     // } else {
-    //     links.classList.add("show-links");
+    //     links.classList.add("show-links")
     // }
 
     // shortcut

9497b848432d1392b62b351859be9e0f6770e350
Akash Sharma <soulakash20002@gmail.com>
2024-12-08 16:36:04 +0530

    Update

--- Backend/My02_NPM/manage_data.js
+++ Backend/My02_NPM/manage_data.js
@@ -1,30 +1,29 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.method === "GET" && req.url === "/notes") {
-        const data = fs.readFileSync("./notes.json", "utf-8");
-        res.setHeader("Content-Type", "application/json");
+    if (req.method === 'GET' && req.url === '/notes') {
+        const data = fs.readFileSync('./notes.json', 'utf-8');
+        res.setHeader('Content-Type', 'application/json');
         res.end(data);
-    } else if (req.method === "POST" && req.url === "/notes") {
-        let body = "";
-        req.on("data", chunk => {
+    } else if (req.method === 'POST' && req.url === '/notes') {
+        let body = '';
+        req.on('data', chunk => {
             body += chunk.toString();
         });
-
-        req.on("end", () => {
-            const notes = JSON.parse(fs.readFileSync("./notes.json", "utf-8"));
+        req.on('end', () => {
+            const notes = JSON.parse(fs.readFileSync('./notes.json', 'utf-8'));
             const newNote = JSON.parse(body);
             notes.push(newNote);
-            fs.writeFileSync("./notes.json", JSON.stringify(notes));
-            res.end("Note Added Successfully");
+            fs.writeFileSync('notes.json', JSON.stringify(notes));
+            res.end('Note added successfully');
         });
     } else {
         res.statusCode = 404;
-        res.end("Error");
+        res.end('Not Found');
     }
 });
 
 server.listen(3000, () => {
-    console.log("Server listening at http://localhost:3000/");
-})
+    console.log('Server running at http://localhost:3000/');
+});

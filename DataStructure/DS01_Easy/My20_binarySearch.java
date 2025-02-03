f69ffefdd621431fab2833b241591832a082402f
Akash Sharma <soulakash20002@gmail.com>
2025-02-03 20:48:46 +0530

    Binary Search

---
+++ DataStructure/DS01_Easy/My20_binarySearch.java
@@ -0,0 +1,23 @@
+public class My20_binarySearch {
+    
+    static int binarySearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        System.out.println(binarySearch(arr, 10));
+    }
+}

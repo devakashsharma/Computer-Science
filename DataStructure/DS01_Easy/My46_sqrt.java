58e14aa6c7dcfd969d3ac1cf235ecdeaf15326a7
Akash Sharma <soulakash20002@gmail.com>
2025-02-25 18:58:06 +0530

    Floor Square

--- DataStructure/DS01_Easy/My46_sqrt.java
+++ DataStructure/DS01_Easy/My46_sqrt.java
@@ -14,8 +14,26 @@ public class My46_sqrt {
         return ans;
     }
 
+    // Optimal
+    static int optimalFloorSqrt(int n) {
+        int low = 1, high = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long val = (long) mid * mid;
+            if (val <= n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return high;
+    }
+
     public static void main(String[] args) {
         int n = 10;
-        System.out.println(floorSqrt(n));
+        // System.out.println(floorSqrt(n));
+        System.out.println(optimalFloorSqrt(n));
     }
 }

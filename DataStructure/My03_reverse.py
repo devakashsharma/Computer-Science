682f9da15f9a3009ff71509bed14b36a98fc18a3
Akash Sharma <soulakash20002@gmail.com>
2025-08-08 16:26:38 +0530

    reverse array

---
+++ DataStructure/My03_reverse.py
@@ -0,0 +1,11 @@
+# Reverse an array.
+arr = [1, 2, 3, 4, 5]
+rev_arr = []
+
+for i in range(len(arr) - 1, -1, -1):
+    rev_arr.append(arr[i])
+
+print(rev_arr)
+
+newArr = rev_arr[::-1]
+print(newArr)

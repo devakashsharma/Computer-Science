402820d87fa05b5f450d2e63306c2669f4bd003e
Akash Sharma <soulakash20002@gmail.com>
2025-10-05 16:28:05 +0530

    pattern 1

---
+++ DataStructure/Step 00 - Patterns/pattern1.py
@@ -0,0 +1,13 @@
+# def print_star():
+#     for i in range(0, 5):
+#         print("*", end=" ")
+
+# num = 1
+# while num <= 5:
+#     print_star()
+#     print("")
+#     num = num + 1
+
+# The concise, Pythonic way
+for _ in range(5):
+    print("* " * 5)

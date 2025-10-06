04f074fe4e7492fabab8c06612d91bc80853e3f6
Akash Sharma <soulakash20002@gmail.com>
2025-10-06 23:07:46 +0530

    update

--- DataStructure/Step 00 - Patterns/pattern11.py
+++ DataStructure/Step 00 - Patterns/pattern11.py
@@ -1,8 +1,9 @@
 val = 1
-for i in range(1, 6
+for i in range(1, 6):
+    # wtf? we can define like this in py? I really love this one
     val = 1 if i % 2 != 0 else 0
 
-    for j in range (i):
+    for j in range(i):
         print(val, end=" ")
         val = 1 - val
     print()
